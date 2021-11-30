93486305c0e9934f1a27a8f6ad739d5609ca7f6b
mikechurvis <michael@productivityenhancement>
2021-11-30 12:40:33 -0500

    Completed Tutorial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Django + Celery
+
+A demonstration of asynchronous tasks using Django, Celery, and Redis. Containerized; requires Docker to run.
+
+Follows a tutorial: https://testdriven.io/blog/django-and-celery/
--- project/tests/test_tasks.py
+++ project/tests/test_tasks.py
@@ -1,7 +1,26 @@
-from django.urls import reverse
+from unittest.mock import patch
 
+from django.urls import reverse
+from tasks import sample_tasks
 
 def test_home(client):
     url = reverse("home")
     response = client.get(url)
     assert response.status_code == 200
+
+
+def test_task():
+    assert sample_tasks.create_task.run(1)
+    assert sample_tasks.create_task.run(2)
+    assert sample_tasks.create_task.run(3)
+
+@patch("tasks.sample_tasks.create_task.run")
+def test_mock_test(mock_run):
+    assert sample_tasks.create_task.run(1)
+    sample_tasks.create_task.run.assert_called_once_with(1)
+
+    assert sample_tasks.create_task.run(2)
+    assert sample_tasks.create_task.run.call_count == 2
+
+    assert sample_tasks.create_task.run(3)
+    assert sample_tasks.create_task.run.call_count == 3
